5287b809a8f63e3b6f2803cfb01a3b5010a242b6
hakan-ilhan <hakan.can.ilhan@gmail.com>
2024-03-26 14:19:45 +0300

    S14D2-Solution1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Bedroom bedroom = new Bedroom("IDK",new Wall("North"),new Wall("South"),new Wall("West"),new Wall("North"),new Ceiling(6, PaintColor.WHITE),new Bed("IDK2",5,6,7,8),
+                new Lamp(LampType.NORMAL,true, 80),new Wardrobe(8,4,100.0),new Carpet(2,2,PaintColor.WHITE));
+
+        System.out.println(bedroom.getLamp().toString());
     }
 }
---
+++ src/main/java/org/example/model/Bed.java
@@ -0,0 +1,37 @@
+package org.example.model;
+
+public class Bed {
+    private String style;
+    private int pillows, height, sheets;
+    private  int  quilts;
+
+    public Bed(String style, int pillows, int height, int sheets, int quilt) {
+        this.style = style;
+        this.pillows = pillows;
+        this.height = height;
+        this.sheets = sheets;
+        this.quilts = quilt;
+    }
+    public void make(){
+        System.out.println("The bed is being made.");
+    }
+    public int getQuilts(){
+        return quilts;
+    }
+    public int getPillows() {
+        return pillows;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getSheets() {
+        return sheets;
+    }
+    public String getStyle(){
+        return style;
+    }
+
+
+}
